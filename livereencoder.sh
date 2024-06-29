58bd7072e41e32e50eedd67151786b53877b0599
ScarlettPPC <131929348+ScarlettPPC@users.noreply.github.com>
2024-06-29 22:59:20 +0300

    Add livereencoder

---
+++ livereencoder.sh
@@ -0,0 +1,15 @@
+echo "Live Re-encoder for low bandwidth"
+echo "By Mueller Minki"
+echo "Wrapper script by ScarlettPPC"
+
+echo "Enter YouTube/TikTok/media link:"
+read _link
+
+echo "Enter quality (low, mid or high):"
+read _quality
+
+
+echo http://muellers-software.org/live-encode/video/mp4-h264.php?quality=${_quality}&url=${_link}
+curl "http://muellers-software.org/live-encode/video/mp4-h264.php?quality=${_quality}&url=${_link}" | ffplay -
+
+
